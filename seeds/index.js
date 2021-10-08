777881e8949283a89ec02d0eeb49b606d414ca50
Satyajit <satyajit.1418@gmail.com>
2021-10-08 20:31:59 +0530

    Image Addded

--- seeds/index.js
+++ seeds/index.js
@@ -18,9 +18,13 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 1; i <= 50; i++){
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://source.unsplash.com/collection/483251',
+            description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestias suscipit necessitatibus officia maxime, dolor ipsum nemo eius animi amet! Architecto fugiat nulla hic obcaecati pariatur! Iusto eius sint labore harum.',
+            price
         })
         await camp.save();
     }
